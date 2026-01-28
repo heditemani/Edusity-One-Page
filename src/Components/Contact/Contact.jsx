b8e4181a5b642a6364d58f000d73eefe72edd6f5
Hedi Temani <hedi.temani97@gmail.com>
2026-01-28 19:49:49 +0100

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        formData.append("access_key", "b9a6afe9-6cc6-473d-847f-9199418edebf");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+        });
+
+        const data = await response.json();
+        setResult(data.success ? "Email send Successfuly !" : "Error");
+    };
+
+
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3><img src={msg_icon} alt="" />Send us a message</h3>
+            <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+            <ul>
+                <li><img src={mail_icon} alt="" />hedi.temani97@gmail.com</li>
+                <li><img src={phone_icon} alt="" />+216 56 027 194</li>
+                <li><img src={location_icon} alt="" />Korba 8070, Nabeul <br /> Tunisia</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your Name</label>
+                <input type="text" name='name' placeholder='Enter your name' required />
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your mobile number' required />
+                <label>Write your messages here</label>
+                <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+                <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
+            </form>
+            <span>{result}</span>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
